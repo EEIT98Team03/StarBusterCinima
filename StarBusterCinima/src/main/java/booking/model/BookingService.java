2e66787c028aeafc38144428aa0d3fb84cc2c891
周栩鈴 <lindaxlzh@gmail.com>
2017-12-28 21:21:00 +0800

    linda
    linda

--- StarBusterCinima/src/main/java/booking/model/BookingService.java
+++ StarBusterCinima/src/main/java/booking/model/BookingService.java
@@ -1,25 +1,103 @@
 package booking.model;
 
 import java.sql.Timestamp;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.TimeZone;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 public class BookingService {
 
-//	BookingDAO bookingDAO;
+	@Autowired
+	SessionFactory sessionFactory;
 	
+	public Session session() {
+		return sessionFactory.getCurrentSession();
+		
+	}
+	
+//	BookingDAO bookingDAO;
+	@Transactional
 	public TicketOrderBean insertOrder (TicketOrderBean bean) {
 //		SimpleDateFormat date =new SimpleDateFormat("yyyy-MM-dd");
 //		date.setTimeZone(TimeZone.getTimeZone("GMT+8"));
 //		String str = date.format(new java.util.Date());
 //		bean.setTicketOrderDate(str);
+
+		session().save(bean);
 		
 		
 		
 		return bean;
 	}
+	
+	@SuppressWarnings("unused")
+	public Timestamp toTimeStamp(String str) {
+//		TimeZone timezone = TimeZone.getTimeZone("GMT+8");
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
+		Date udate = null;
+		Timestamp timeStamp =null;
+		if(udate!=null) {
+			try {
+				udate = sdf.parse(str.trim().replace(" ", ""));
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			timeStamp = new Timestamp(udate.getTime());
+		}
+//		System.out.println(date.getClass().getName());
+//		SimpleDateFormat sdf = new SimpleDateFormat("");
+//		ticketOrderBean.setTicketOrderDate(newTicketOrderDate);
+		
+		return timeStamp ;
+	}
+	
+	
+	@SuppressWarnings("unused")
+	public Timestamp orderFilmsectiontime(String str) {
+		SimpleDateFormat sdfParse = new SimpleDateFormat("M月d日,EEE.hh:mm?yyyy");
+//		SimpleDateFormat sdfParse = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+		sdfParse.setTimeZone(TimeZone.getTimeZone("GMT+8"));
+
+//		str = "1991-10-10 10:10";
+//		System.out.println("test "+str);
+		
+//		1月7日,星期日.01:40?2018
+		
+		
+		Date udate =null;
+		if(udate !=null) {
+			try {
+				str = str+"-2018";
+				str = str.replace('(',',').replace('-','?').trim();
+				udate = sdfParse.parse(str);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+			Timestamp timeStamp = new Timestamp(udate.getTime());
+			
+			return timeStamp;
+		}
+		return null;
+	}
+	
+	public String processSeatNum(String str) {
+		
+		str = str.replace("號", "號,");
+		
+		return str;
+	}
+	
+	
+	
 	
 }
--- StarBusterCinima/src/main/java/booking/model/TicketOrderBean.java
+++ StarBusterCinima/src/main/java/booking/model/TicketOrderBean.java
@@ -1,55 +1,51 @@
 package booking.model;
 
-import java.sql.Date;
 import java.sql.Timestamp;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 @Entity
 @Table(name = "TICKETORDER")
 public class TicketOrderBean {
 	
 	
 	@Id
-	private int ticketOrderID;
-	private Date ticketOrderDate;
-	private int memberID;
-	private int filmID;
-	private String seatNum;
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int ticketOrderId;
+	private Timestamp ticketOrderDate;
+	private Timestamp filmsectiontime;
 	private int ticketCount;
-	private int ticketOrderAmount;
+	private String seatNum;
+	private int ticketOrderPrice;
 	private int ticketState;
-	public int getTicketOrderID() {
-		return ticketOrderID;
+	private int filmId;
+	private int memberId;
+	public int getTicketOrderId() {
+		return ticketOrderId;
 	}
-	public void setTicketOrderID(int ticketOrderID) {
-		this.ticketOrderID = ticketOrderID;
+	public void setTicketOrderId(int ticketOrderId) {
+		this.ticketOrderId = ticketOrderId;
 	}
-	public Date getTicketOrderDate() {
+	public Timestamp getTicketOrderDate() {
 		return ticketOrderDate;
 	}
-	public void setTicketOrderDate(Date ticketOrderDate) {
+	public void setTicketOrderDate(Timestamp ticketOrderDate) {
 		this.ticketOrderDate = ticketOrderDate;
 	}
-	public int getMemberID() {
-		return memberID;
+
+
+	public Timestamp getFilmsectiontime() {
+		return filmsectiontime;
 	}
-	public void setMemberID(int memberID) {
-		this.memberID = memberID;
-	}
-	public int getFilmID() {
-		return filmID;
-	}
-	public void setFilmID(int filmID) {
-		this.filmID = filmID;
-	}
-	public String getSeatNum() {
-		return seatNum;
-	}
-	public void setSeatNum(String seatNum) {
-		this.seatNum = seatNum;
+	public void setFilmsectiontime(Timestamp filmsectiontime) {
+		this.filmsectiontime = filmsectiontime;
 	}
 	public int getTicketCount() {
 		return ticketCount;
@@ -57,11 +53,18 @@ public class TicketOrderBean {
 	public void setTicketCount(int ticketCount) {
 		this.ticketCount = ticketCount;
 	}
-	public int getTicketOrderAmount() {
-		return ticketOrderAmount;
+	public String getSeatNum() {
+		return seatNum;
 	}
-	public void setTicketOrderAmount(int ticketOrderAmount) {
-		this.ticketOrderAmount = ticketOrderAmount;
+	public void setSeatNum(String seatNum) {
+		this.seatNum = seatNum;
+	}
+
+	public int getTicketOrderPrice() {
+		return ticketOrderPrice;
+	}
+	public void setTicketOrderPrice(int ticketOrderPrice) {
+		this.ticketOrderPrice = ticketOrderPrice;
 	}
 	public int getTicketState() {
 		return ticketState;
@@ -69,36 +72,90 @@ public class TicketOrderBean {
 	public void setTicketState(int ticketState) {
 		this.ticketState = ticketState;
 	}
+	public int getFilmId() {
+		return filmId;
+	}
+	public void setFilmId(int filmId) {
+		this.filmId = filmId;
+	}
+	public int getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(int memberId) {
+		this.memberId = memberId;
+	}
 	@Override
 	public String toString() {
-		return "TicketOrderBean [ticketOrderID=" + ticketOrderID + ", ticketOrderDate=" + ticketOrderDate
-				+ ", memberID=" + memberID + ", filmID=" + filmID + ", seatNum=" + seatNum + ", ticketCount="
-				+ ticketCount + ", ticketOrderAmount=" + ticketOrderAmount + ", ticketState=" + ticketState + "]";
+		return "TicketOrderBean [ticketOrderId=" + ticketOrderId + ", ticketOrderDate=" + ticketOrderDate
+				+ ", filmsectiontime=" + filmsectiontime + ", ticketCount=" + ticketCount + ", seatNum=" + seatNum
+				+ ", ticketOrderPrice=" + ticketOrderPrice + ", ticketState=" + ticketState + ", filmId=" + filmId
+				+ ", memberId=" + memberId + "]";
 	}
+
 	
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-//	private String ticketOrderId;
-//	private Date ticketOrderDate;
-//	private int ticketOrderAmount;
-//	private int memberID;
-//	private String filmId;
-//	private String ticketState;
-//	private int ticketCount;
-//	private String seatNumber;
-	
-	
-	
-	
+//	public int getTicketorderid() {
+//		return ticketorderid;
+//	}
+//	public void setTicketorderid(int ticketorderid) {
+//		this.ticketorderid = ticketorderid;
+//	}
+//	public Timestamp getTicketorderdate() {
+//		return ticketorderdate;
+//	}
+//	public void setTicketorderdate(Timestamp ticketorderdate) {
+//		this.ticketorderdate = ticketorderdate;
+//	}
+//	public Timestamp getDatetime() {
+//		return datetime;
+//	}
+//	public void setDatetime(Timestamp datetime) {
+//		this.datetime = datetime;
+//	}
+//	public int getTicketcount() {
+//		return ticketcount;
+//	}
+//	public void setTicketcount(int ticketcount) {
+//		this.ticketcount = ticketcount;
+//	}
+//	public String getSeatnum() {
+//		return seatnum;
+//	}
+//	public void setSeatnum(String seatnum) {
+//		this.seatnum = seatnum;
+//	}
+//	public int getTicketorderamount() {
+//		return ticketorderamount;
+//	}
+//	public void setTicketorderamount(int ticketorderamount) {
+//		this.ticketorderamount = ticketorderamount;
+//	}
+//	public int getTicketstate() {
+//		return ticketstate;
+//	}
+//	public void setTicketstate(int ticketstate) {
+//		this.ticketstate = ticketstate;
+//	}
+//	public int getFilmid() {
+//		return filmid;
+//	}
+//	public void setFilmid(int filmid) {
+//		this.filmid = filmid;
+//	}
+//	public int getMemberid() {
+//		return memberid;
+//	}
+//	public void setMemberid(int memberid) {
+//		this.memberid = memberid;
+//	}
+//	@Override
+//	public String toString() {
+//		return "TicketOrderBean [ticketorderid=" + ticketorderid + ", ticketorderdate=" + ticketorderdate
+//				+ ", datetime=" + datetime + ", ticketcount=" + ticketcount + ", seatnum=" + seatnum
+//				+ ", ticketorderamount=" + ticketorderamount + ", ticketstate=" + ticketstate + ", filmid=" + filmid
+//				+ ", memberid=" + memberid + "]";
+//	}
 
 	
 	
